77f2fac1e1b6cda00055a07e8cca54e057e3a593
denslatv <denslats@gmail.com>
2022-11-05 23:14:36 +0200

    mp3 objects function 2nd vers

--- script/script4.js
+++ script/script4.js
@@ -1,25 +1,27 @@
-let mp3 = {
-    number_of_tracks:0,
-    currentTrack : 0,
-    tracks: [],
-    addtrack : function (music){
-        this.tracks.push(music);
-        this.number_of_tracks++;
-    },
-    random : function (){
-        Math.floor(Math.random() * this.number_of_tracks);
-    },
-    previousTrack : function (){
-        --this.currentTrack;
-    },
-    nextTrack : function (){
-        ++this.currentTrack;
-    },
-    setTrack : function (number){
-        this.currentTrack = number;
-    },
-    printTrack : function (){
-        console.log("The current track is " + this.currentTrack);
+function createPlayer(number) {
+    return {
+        number_of_tracks: number,
+        currentTrack: 0,
+        tracks: [],
+        addtrack: function (music) {
+            this.tracks.push(music);
+        },
+        random: function () {
+           this.currentTrack = Math.floor(Math.random() * this.number_of_tracks);
+           console.log(this.currentTrack);
+        },
+        previousTrack: function () {
+            --this.currentTrack;
+        },
+        nextTrack: function () {
+            ++this.currentTrack;
+        },
+        setTrack: function (number) {
+            this.currentTrack = number;
+        },
+        printTrack: function () {
+            console.log("The current track is " + this.currentTrack);
+        }
     }
 }
 let track1 ={
@@ -38,6 +40,7 @@ let track2 ={
         console.log("The track" + this.name + " " + this.author + " " + this.length);
     }
 }
+let mp3 = createPlayer(2);
 mp3.addtrack(track1);
 mp3.addtrack(track2);
 mp3.printTrack();
@@ -45,6 +48,18 @@ mp3.tracks[mp3.currentTrack].print();
 mp3.nextTrack();
 mp3.printTrack();
 mp3.tracks[mp3.currentTrack].print();
-mp3.printTrack();
 mp3.random();
+mp3.printTrack();
 mp3.tracks[mp3.currentTrack].print();
+
+let mp3_1 = createPlayer(2);
+mp3_1.addtrack(track1);
+mp3_1.addtrack(track2);
+mp3_1.printTrack();
+mp3_1.tracks[mp3_1.currentTrack].print();
+mp3_1.nextTrack();
+mp3_1.printTrack();
+mp3_1.tracks[mp3_1.currentTrack].print();
+mp3_1.random();
+mp3_1.printTrack();
+mp3_1.tracks[mp3_1.currentTrack].print();
